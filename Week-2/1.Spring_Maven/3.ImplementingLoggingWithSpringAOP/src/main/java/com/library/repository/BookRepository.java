0dca54e6590a43f01fa6edd9d0cf79a380ef091b
palakg12 <115335427+palakg12@users.noreply.github.com>
2024-08-07 16:35:45 +0530

    Delete Week-2/1.Spring_Maven directory

--- Week-2/1.Spring_Maven/3.ImplementingLoggingWithSpringAOP/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-
-    // Method to simulate fetching books
-    public void fetchBooks() {
-        System.out.println("Fetching books from the repository...");
-    }
-}
