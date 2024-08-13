ed027da438d39fb3b8b8e93eb932a7f7de417a8b
palakg12 <115335427+palakg12@users.noreply.github.com>
2024-08-14 00:38:20 +0530

    Add files via upload

---
+++ Week-2/Week-3/Excercise-2/src/main/java/com/example/EmployeeManagementSystem/entity/Employee.java
@@ -0,0 +1,24 @@
+package com.example.EmployeeManagementSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
