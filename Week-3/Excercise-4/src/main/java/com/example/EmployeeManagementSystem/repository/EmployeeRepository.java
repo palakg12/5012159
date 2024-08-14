d15a1936022ef8ab9f4480e0b2ee40fcf26f3d69
palakg12 <115335427+palakg12@users.noreply.github.com>
2024-08-15 00:26:55 +0530

    Delete Week-3/Excercise-4 directory

--- Week-3/Excercise-4/src/main/java/com/example/EmployeeManagementSystem/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.EmployeeManagementSystem.repository;
-
-import com.example.EmployeeManagementSystem.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    // Derived query methods can be added here if needed
-}
